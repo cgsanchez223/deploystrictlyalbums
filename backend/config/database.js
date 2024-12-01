e96cdf9acf48dde0a93c1aa6ac6c084941f9add4
Christopher Sanchez <cgsanchez223@gmail.com>
2024-12-01 18:47:16 -0500

    user error

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.PG_DATABASE,
+    username: process.env.PG_PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     host: process.env.PG_HOST,
     dialect: 'postgres'
   },
   test: {
-    username: process.env.PG_DATABASE,
+    username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE_TEST,
     host: process.env.PG_HOST,
     dialect: 'postgres'
   },
   production: {
-    username: process.env.PG_DATABASE,
+    username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     host: process.env.PG_HOST,
